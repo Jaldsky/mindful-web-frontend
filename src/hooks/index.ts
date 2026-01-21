d5ad98c5561eee4e3d77710566ddd807c74b5fcf
Artem Ryzhenkov <ryzhenkov.artem2@wb.ru>
2026-01-21 21:02:54 +0300

    Update hooks

---
+++ src/hooks/index.ts
@@ -0,0 +1,21 @@
+/**
+ * Custom Hooks
+ * Centralized export for all custom hooks
+ */
+
+export { useAnalytics, useChartDimensions, useDateRange } from './analytics';
+export { useAuthAnimation } from './auth';
+export { useTranslation, useTimezone } from './i18n';
+export type {
+  UseAnalyticsReturn,
+  AnalyticsRequestParams,
+  AnalyticsUsageResponse,
+  UseAuthAnimationReturn,
+  ChartDimensions,
+  DateRange,
+  UseDateRangeReturn,
+  UseTimezoneReturn,
+  UseTranslationReturn,
+} from './types';
+export { timezoneDetector, translationService } from './utils';
+export type { TimezoneDetector, TranslationService } from './utils';
