9b62374c2ea77594ef751301003c1a9b977a8c43
Artem Ryzhenkov <ryzhenkov.artem2@wb.ru>
2026-01-26 11:11:23 +0300

    Add footer + tests

--- web/src/components/layout/Layout.tsx
+++ web/src/components/layout/Layout.tsx
@@ -1,19 +1,32 @@
 import React from 'react';
 import { Header } from './header';
+import { Footer } from './footer';
 import { LAYOUT_STYLES } from './constants';
 import type { LayoutProps } from './types';
 
 export const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="min-h-screen" style={LAYOUT_STYLES.container}>
+    <div
+      className="min-h-screen"
+      style={{
+        ...LAYOUT_STYLES.container,
+        display: 'flex',
+        flexDirection: 'column',
+      }}
+    >
       <Header />
 
       <main
-        className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 py-8"
-        style={LAYOUT_STYLES.main}
+        className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 py-8 w-full"
+        style={{
+          ...LAYOUT_STYLES.main,
+          flex: 1,
+        }}
       >
         {children}
       </main>
+
+      <Footer />
     </div>
   );
 };
--- web/src/components/layout/index.ts
+++ web/src/components/layout/index.ts
@@ -1,4 +1,5 @@
 export { Layout } from './Layout';
 export { Header, AuthButton, Navigation, NavigationItem, HeaderControls, BurgerButton } from './header';
+export { Footer } from './footer';
 export { Sidebar } from './sidebar';
 export * from './constants';
